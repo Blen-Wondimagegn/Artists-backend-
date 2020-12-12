7d67b87e09d0e731b63cf420e355b982e61d692d
blen_meku <mekuriablen@gmail.com>
2020-12-12 00:40:40 -0800

    add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :glams
-  resources :artists
+ resources :glams
+  resources :artists do
+    resources :glams 
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
