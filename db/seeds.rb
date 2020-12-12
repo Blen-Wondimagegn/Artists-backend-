e092f468e5967fed1785ff08e2ae76ad217076f2
blen_meku <mekuriablen@gmail.com>
2020-12-11 21:27:57 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,112 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times {
+    Artist.create([{
+        name: Faker::Kpop.solo, 
+        gender: Faker::Demographic.sex,
+        height: Faker::Demographic.height(unit: :imperial), 
+        age: Faker::Number.within(range: 20..35),
+        image:"./kpop-imgs/sm-reveals-identity-of-the-third-aespa-member_.jpg"
+      }])
+ }
+
+3.times {
+    Glam.create([{
+        glam_squad: Faker::Space.star,
+        makeup:Faker::Name.first_name, 
+        hair: Faker::Name.middle_name, 
+        wardrobe: Faker::Name.initials, 
+        artist_id: 1
+       }])
+  }
+
+3.times {
+    Glam.create([{
+        glam_squad: Faker::Space.star, 
+        makeup:Faker::Name.first_name, 
+        hair: Faker::Name.middle_name, 
+        wardrobe: Faker::Name.initials,
+        artist_id: 2
+        }])
+    }
+
+3.times {
+    Glam.create([{
+        glam_squad: Faker::Space.star,
+        makeup:Faker::Name.first_name, 
+        hair: Faker::Name.middle_name, 
+        wardrobe: Faker::Name.initials, 
+        artist_id: 3
+     }])
+}
+
+3.times {
+    Glam.create([{
+        glam_squad: Faker::Space.star, 
+        makeup:Faker::Name.first_name, 
+        hair: Faker::Name.middle_name, 
+        wardrobe: Faker::Name.initials, 
+        artist_id: 4
+         }])
+}
+
+3.times {
+    Glam.create([{
+        glam_squad: Faker::Space.star,
+        makeup:Faker::Name.first_name, 
+        hair: Faker::Name.middle_name, 
+        wardrobe: Faker::Name.initials, 
+        artist_id: 5
+       }])
+  }
+
+  3.times {
+    Glam.create([{
+        glam_squad: Faker::Space.star,
+        makeup:Faker::Name.first_name, 
+        hair: Faker::Name.middle_name, 
+        wardrobe: Faker::Name.initials, 
+        artist_id: 6 
+       }])
+  }
+
+  3.times {
+    Glam.create([{
+        glam_squad: Faker::Space.star,
+        makeup:Faker::Name.first_name, 
+        hair: Faker::Name.middle_name, 
+        wardrobe: Faker::Name.initials, 
+        artist_id: 7
+       }])
+  }
+
+  3.times {
+    Glam.create([{
+        glam_squad: Faker::Space.star,
+        makeup:Faker::Name.first_name, 
+        hair: Faker::Name.middle_name, 
+        wardrobe: Faker::Name.initials, 
+        artist_id: 8
+       }])
+  }
+
+  3.times {
+    Glam.create([{
+        glam_squad: Faker::Space.star,
+        makeup:Faker::Name.first_name, 
+        hair: Faker::Name.middle_name, 
+        wardrobe: Faker::Name.initials, 
+        artist_id: 9
+       }])
+  }
+
+  3.times {
+    Glam.create([{
+        glam_squad: Faker::Space.star,
+        makeup:Faker::Name.first_name, 
+        hair: Faker::Name.middle_name, 
+        wardrobe: Faker::Name.initials, 
+        artist_id: 10
+       }])
+  }
