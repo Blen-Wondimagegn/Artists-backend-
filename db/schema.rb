d9467e9c7b09d17deafd39dfb1b8df83c9cc90b7
blen_meku <mekuriablen@gmail.com>
2020-12-15 21:56:15 -0800

    modfied

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_041647) do
+ActiveRecord::Schema.define(version: 2020_12_14_224011) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_12_12_041647) do
     t.integer "artist_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "comments"
   end
 
 end
